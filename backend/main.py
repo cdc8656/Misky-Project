4e90ad7df6080653119783c50cd9f5fcff4d7345
cdc8656 <cdc8656@nyu.edu>
2025-07-24 02:52:44 -0400

    cores fix

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://misky-project.onrender.com",
-        "https://misky-project.vercel.app"],  # update as needed for deployment
+        "https://misky-project-1cj4.vercel.app"],  # update as needed for deployment
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
